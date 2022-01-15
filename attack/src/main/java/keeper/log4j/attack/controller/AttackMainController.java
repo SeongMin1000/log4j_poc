bf90ca093e065583c6d434362b8954fd8efd4ee4
1004-head <woduf0628@naver.com>
2022-01-15 14:35:54 +0900

    스프링 프로젝트 생성

---
+++ attack/src/main/java/keeper/log4j/attack/controller/AttackMainController.java
@@ -0,0 +1,5 @@
+package keeper.log4j.attack.controller;
+
+public class AttackMainController {
+
+}
---
+++ attack/src/test/java/keeper/log4j/attack/AttackApplicationTests.java
@@ -0,0 +1,13 @@
+package keeper.log4j.attack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
