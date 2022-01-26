aecc502788b969128c2fd7e215f20d2cf1f8b7a3
Doe <do6065@pusan.ac.kr>
2022-01-26 15:19:38 +0900

    db에서 최근데이터 5개만 띄우기

--- attack/src/main/java/keeper/log4j/attack/repository/AttackRepository.java
+++ attack/src/main/java/keeper/log4j/attack/repository/AttackRepository.java
@@ -1,13 +1,14 @@
 package keeper.log4j.attack.repository;
 
-import keeper.log4j.attack.controller.AttackMainController;
 import keeper.log4j.attack.entity.AttackEntity;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AttackRepository extends JpaRepository<AttackEntity, Long> {
+
+
 }
 
